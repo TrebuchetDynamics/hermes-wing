58c77c186892397e66b57a6a7e89012906462fcf
xel <tamez.jm@gmail.com>
2026-05-31 13:05:13 -0600

    Refactor lib/features/servers/pairing topology

--- lib/features/servers/pairing/pairing_handoff_manual_edit.dart
+++ lib/features/servers/pairing/pairing_handoff_manual_edit.dart
@@ -1,10 +1,1 @@
-enum PairingHandoffManualEdit { address, port, token }
-
-bool pairingHandoffManualEditClearsImportedTarget(
-  PairingHandoffManualEdit edit,
-) {
-  return switch (edit) {
-    PairingHandoffManualEdit.address || PairingHandoffManualEdit.port => true,
-    PairingHandoffManualEdit.token => false,
-  };
-}
+export 'contracts/pairing_handoff_manual_edit.dart';
